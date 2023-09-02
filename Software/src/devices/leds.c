b0852773c299fb8889550244865c05de81d2d784
Đorđe Milanović <djordje200179@gmail.com>
2023-09-02 18:24:26 +0200

    LED bus interface improved

--- Software/src/devices/leds.c
+++ Software/src/devices/leds.c
@@ -1,23 +1,24 @@
 #include "devices/leds.h"
 
-static char* ctrl_addr = (char*) 0x70000000;
-static char* data_addr = (char*) 0x70000008;
+static uint32_t* ctrl_reg = (uint32_t*) 0x70000000;
+static uint32_t* data_reg = (uint32_t*) 0x70000008;
 
 void leds_on(void) {
-    *ctrl_addr = 0b1;
+    *ctrl_reg = 0b1;
 }
 
 void leds_off(void) {
-    *ctrl_addr = 0b0;
+    *ctrl_reg = 0b0;
 }
 
 void leds_set_single(uint8_t led, bool state) {
-    if (state)
-        *data_addr |= (1 << led);
-    else
-        *data_addr &= ~(1 << led);
+    ((uint8_t*)data_reg)[led] = state;
 }
 
 void leds_set(uint8_t states) {
-    *data_addr = states;
+    uint32_t data = 0;
+    for (int i = 0; i < 4; i++)
+        data |= ((states >> i) & 0b1) << (i * 8);
+    
+    *data_reg = data;
 }
