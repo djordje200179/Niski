1a7bf4c175a45873739fd77f7e11c263979b7f2a
Đorđe Milanović <djordje200179@gmail.com>
2023-09-04 15:24:59 +0200

    More CSRs added

--- Hardware/CPU/CSRs.v
+++ Hardware/CPU/CSRs.v
@@ -7,15 +7,33 @@ module cpu_csrs (
 	output reg [31:0] data_out,
 	input wr, incr_inst_count
 );
-	localparam CYCLE_ADDR = 12'hC00,
-			   CYCLEH_ADDR = 12'hC80,
-			   TIME_ADDR = 12'hC01,
-			   TIMEH_ADDR = 12'hC81,
-			   INSTRET_ADDR = 12'hC02,
-			   INSTRETH_ADDR = 12'hC82;
+	localparam CYCLE_ADDR		= 12'hC00,
+			   CYCLEH_ADDR 		= 12'hC80,
+			   TIME_ADDR 		= 12'hC01,
+			   TIMEH_ADDR 		= 12'hC81,
+			   INSTRET_ADDR		= 12'hC02,
+			   INSTRETH_ADDR	= 12'hC82;
+
+	localparam SSTATUS_ADDR 	= 12'h100,
+			   SIE_ADDR			= 12'h104,
+			   STVEC_ADDR		= 12'h105,
+			   SCOUNTEREN_ADDR	= 12'h106,
+			   SENVCVG_ADDR		= 12'h10A,
+			   SSCRATCH_ADDR	= 12'h140,
+			   SEPC_ADDR		= 12'h141,
+			   SCAUSE_ADDR		= 12'h142,
+			   STVAL_ADDR		= 12'h143,
+			   SIP_ADDR			= 12'h144,
+			   SATP_ADDR		= 12'h180,
+			   SCONTEXT_ADDR	= 12'h5A8;
+
+	// TODO: Add performance counters (hpmcounter3-hpmcounter31)
+	// TODO: Add scounteren, senvcfg, scontext, satp
 
 	reg [63:0] CYCLE = 64'h0,
 			   TIME, INSTRET;
+
+	reg [31:0] sstatus, sie, stvec, sscratch, sepc, scause, stval, sip;
 
 	always @* begin
 		data_out = 32'b0;
@@ -27,6 +45,14 @@ module cpu_csrs (
 		TIMEH_ADDR:     data_out = TIME[63:32];
 		INSTRET_ADDR:   data_out = INSTRET[31:0];
 		INSTRETH_ADDR:  data_out = INSTRET[63:32];
+		SSTATUS_ADDR:	data_out = sstatus;
+		SIE_ADDR:      	data_out = sie;
+		STVEC_ADDR:    	data_out = stvec;
+		SSCRATCH_ADDR: 	data_out = sscratch;
+		SEPC_ADDR:     	data_out = sepc;
+		SCAUSE_ADDR:   	data_out = scause;
+		STVAL_ADDR:    	data_out = stval;
+		SIP_ADDR:      	data_out = sip;
 		endcase
 	end
 
@@ -40,8 +66,16 @@ module cpu_csrs (
 	task on_clock;
 		begin
 			if (wr) begin
-				// case (addr)
-				// endcase
+				case (addr)
+				SSTATUS_ADDR:	sstatus <= data_in;
+				SIE_ADDR:		sie <= data_in;
+				STVEC_ADDR:		stvec <= data_in;
+				SSCRATCH_ADDR:	sscratch <= data_in;
+				SEPC_ADDR:		sepc <= data_in;
+				SCAUSE_ADDR:	scause <= data_in;
+				STVAL_ADDR:		stval <= data_in;
+				SIP_ADDR:		sip <= data_in;
+				endcase
 			end
 
 			CYCLE <= CYCLE + 32'b1;
