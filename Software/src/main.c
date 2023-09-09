f0f5a9e6af16afd1901c475ed5a7304eacd22f5b
Đorđe Milanović <djordje200179@gmail.com>
2023-09-09 14:55:04 +0200

    Test improved

--- Software/src/main.c
+++ Software/src/main.c
@@ -1,23 +1,46 @@
 #include <devices/ssds.h>
 #include <stddef.h>
+#include <stdbool.h>
 #include <threads.h>
 
-int digit_thread(void* arg) {
-	ssds_set_digit(2, 2);
+mtx_t mtx;
+bool flag[4];
+cnd_t cond[5];
 
+int digit_thread(void* arg) {
+	uintptr_t id = (uintptr_t)arg;
+
+	mtx_lock(&mtx);
+
+	if (!flag[id])
+		cnd_wait(&cond[id], &mtx);
+
+	ssds_set_digit(id, id);
+
+	if (id < 3) {
+		flag[id + 1] = true;
+		cnd_signal(&cond[id + 1]);
+	}
+
+	mtx_unlock(&mtx);
+	
 	return 0;
 }
 
 void main() {
 	ssds_on();
-	ssds_set_digit(0, 1);
 
-	thrd_t handler;
-	int res = thrd_create(&handler, digit_thread, NULL);
+	thrd_t threads[4];
 
-	ssds_set_digit(1, res);
+	mtx_init(&mtx, mtx_plain);
+	for (int i = 0; i < 4; i++) {
+		flag[i] = false;
+		cnd_init(&cond[i]);
+		thrd_create(&threads[i], digit_thread, (void*)(uintptr_t)i);
+	}
+	
+	flag[0] = true;
+	cnd_signal(&cond[0]);
 
 	thrd_yield();
-
-	ssds_set_digit(3, 3);
 }
