1af10cf2cd9451a8f31dbd697745776d2898afae
Đorđe Milanović <djordje200179@gmail.com>
2023-09-13 00:02:39 +0200

    Revert "Memory copying functions optimized"
    This reverts commit 2f7c64dde1b6001843c564ed5ad5a5255e537a47.

--- Software/src/std/string/memory_functions.c
+++ Software/src/std/string/memory_functions.c
@@ -1,5 +1,4 @@
 #include <string.h>
-#include <stdint.h>
 
 void* memchr(const void* ptr, int ch, size_t count) {
 	unsigned char* cptr = (unsigned char*)ptr;
@@ -29,31 +28,10 @@ int memcmp(const void* lhs, const void* rhs, size_t count) {
 void* memset(void* dest, int ch, size_t count) {
 	unsigned char* cdest = (unsigned char*)dest;
 
-	while ((uintptr_t)cdest % 4 != 0 && count > 0) {
-		*cdest = ch;
-		cdest++;
-		count--;
-	}
+	for (size_t i = 0; i < count; i++)
+		cdest[i] = ch;
 
-	uint32_t* dest32 = (uint32_t*)cdest;
-
-	uint32_t ch32 = 0;
-	for (uint8_t i = 0; i < 4; i++)
-		ch32 |= ch32 << (8 * i);
-
-	while (count >= 4) {
-		*dest32 = ch32;
-		dest32++;
-		count -= 4;
-	}
-
-	cdest = (unsigned char*)dest32;
-
-	while (count > 0) {
-		*cdest = ch;
-		cdest++;
-		count--;
-	}
+	return dest;
 }
 
 void* memset_explicit(void* dest, int ch, size_t count) {
@@ -64,32 +42,8 @@ void* memcpy(void* restrict dest, const void* restrict src, size_t count) {
 	unsigned char* cdest = (unsigned char*)dest;
 	unsigned char* csrc = (unsigned char*)src;
 
-	while ((uintptr_t)cdest % 4 != 0 && count > 0) {
-		*cdest = *csrc;
-		cdest++;
-		csrc++;
-		count--;
-	}
-
-	uint32_t* dest32 = (uint32_t*)cdest;
-	uint32_t* src32 = (uint32_t*)csrc;
-
-	while (count >= 4) {
-		*dest32 = *src32;
-		dest32++;
-		src32++;
-		count -= 4;
-	}
-
-	cdest = (unsigned char*)dest32;
-	csrc = (unsigned char*)src32;
-
-	while (count > 0) {
-		*cdest = *csrc;
-		cdest++;
-		csrc++;
-		count--;
-	}
+	for (size_t i = 0; i < count; i++)
+		cdest[i] = csrc[i];
 
 	return dest;
 }
@@ -113,13 +67,11 @@ void* memmove(void* dest, const void* src, size_t count) {
 	unsigned char* csrc = (unsigned char*)src;
 
 	if (cdest < csrc) {
-		memcpy(dest, src, count);
-		return dest;
-	}
-
-	while (count > 0) {
-		cdest[count - 1] = csrc[count - 1];
-		count--;
+		for (size_t i = 0; i < count; i++)
+			cdest[i] = csrc[i];
+	} else {
+		for (size_t i = count; i > 0; i--)
+			cdest[i - 1] = csrc[i - 1];
 	}
 
 	return dest;
