e994f8c2f8ed7f63da27a1e856a0df286d56223c
Đorđe Milanović <djordje200179@gmail.com>
2023-10-01 14:35:53 +0200

    Branch signed operands fixed

--- Hardware/CPU/BranchTester.v
+++ Hardware/CPU/BranchTester.v
@@ -10,12 +10,12 @@ module cpu_branch_tester (
 		condition_satisfied = 1'b0;
 
 		case (funct3)
-		INST_BRANCH_FUNCT3_EQ:	 condition_satisfied = operand_a == operand_b;
-		INST_BRANCH_FUNCT3_NEQ: condition_satisfied = operand_a != operand_b;
-		INST_BRANCH_FUNCT3_LT:  condition_satisfied = operand_a < operand_b;
-		INST_BRANCH_FUNCT3_GE:	 condition_satisfied = operand_a >= operand_b;
-		INST_BRANCH_FUNCT3_LTU: condition_satisfied = $signed(operand_a) < $signed(operand_b);
-		INST_BRANCH_FUNCT3_GEU: condition_satisfied = $signed(operand_a) >= $signed(operand_b);
+		INST_BRANCH_FUNCT3_EQ:	condition_satisfied = operand_a == operand_b;
+		INST_BRANCH_FUNCT3_NEQ:	condition_satisfied = operand_a != operand_b;
+		INST_BRANCH_FUNCT3_LT:  condition_satisfied = $signed(operand_a) < $signed(operand_b);
+		INST_BRANCH_FUNCT3_GE:	condition_satisfied = $signed(operand_a) >= $signed(operand_b);
+		INST_BRANCH_FUNCT3_LTU: condition_satisfied = operand_a < operand_b;
+		INST_BRANCH_FUNCT3_GEU: condition_satisfied = operand_a >= operand_b;
 		endcase
 	end
 endmodule
