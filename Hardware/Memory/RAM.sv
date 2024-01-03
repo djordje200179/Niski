a86152ef022401b67860af2d10163059bcf8a4a3
Đorđe Milanović <djordje200179@gmail.com>
2024-01-03 23:12:20 +0100

    Memory modules converted to SV

--- Hardware/Memory/RAM.sv
+++ Hardware/Memory/RAM.sv
@@ -1,19 +1,13 @@
-module ram (
-	clk,
-	wr, wr_mask,
-	addr, data_in, data_out
-);
-	parameter ADDR_BITS = 10;
-	parameter MEM_FILE = "";
+module ram #(ADDR_BITS = 10, MEM_FILE = "") (
+	input clk,
 
-	input clk;
-	input wr;
-	input [3:0] wr_mask;
-	
-	input [ADDR_BITS-1:0] addr;
-	input [31:0] data_in;
-	output reg [31:0] data_out;
-	
+	input wr, 
+	input [3:0] wr_mask,
+
+	input [ADDR_BITS-1:0] addr, 
+	input [31:0] data_in,
+	output reg [31:0] data_out
+);
 	reg [3:0][7:0] memory [2**ADDR_BITS];
 	initial begin
 		if (MEM_FILE != "")
@@ -22,6 +16,7 @@ module ram (
 	
 	always_ff @(posedge clk) begin
 		if (wr) begin
+			// Can't use a for loop here for unknown reason
 			if(wr_mask[0]) memory[addr][0] <= data_in[0+:8];
 			if(wr_mask[1]) memory[addr][1] <= data_in[8+:8];
 			if(wr_mask[2]) memory[addr][2] <= data_in[16+:8];
