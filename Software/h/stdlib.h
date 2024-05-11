166020b20cb17fab12dcce569e692bf3c27ed9fb
Đorđe Milanović <djordje200179@gmail.com>
2024-05-11 21:54:40 +0200

    Random number generator implemented

--- Software/h/stdlib.h
+++ Software/h/stdlib.h
@@ -6,3 +6,7 @@ void* malloc(size_t size);
 void* calloc(size_t num, size_t size);
 
 void free(void* ptr);
+
+int rand(void);
+void srand(unsigned int seed);
+#define RAND_MAX 32767
--- Software/src/std/stdlib/stdlib_functions.c
+++ Software/src/std/stdlib/stdlib_functions.c
@@ -12,3 +12,14 @@ void* calloc(size_t num, size_t size) {
 
 	return ptr;
 }
+
+static unsigned int seed = 0;
+
+int rand(void) {
+	seed = seed * 1103515245 + 12345;
+	return (unsigned int)(seed / 65536) % 32768;
+}
+
+void srand(unsigned int new_seed) {
+	seed = new_seed;
+}
