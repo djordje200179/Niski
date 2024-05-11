916d0cebe7efca9fa9e9a7737543d1d06e6aaf4e
Đorđe Milanović <djordje200179@gmail.com>
2024-05-11 22:01:59 +0200

    Binary search algorithm implemented

--- Software/h/stdlib.h
+++ Software/h/stdlib.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stddef.h>
+#include "errno.h"
 
 void* malloc(size_t size);
 void* calloc(size_t num, size_t size);
@@ -10,3 +11,6 @@ void free(void* ptr);
 int rand(void);
 void srand(unsigned int seed);
 #define RAND_MAX 32767
+
+void* bsearch(const void* key, const void* base, size_t num, size_t size, int (*comp)(const void*, const void*));
+errno_t bsearch_s(const void* key, const void* base, size_t num, size_t size, int (*compare)(void*, const void*, const void*), void* context);
--- Software/src/std/stdlib/stdlib_functions.c
+++ Software/src/std/stdlib/stdlib_functions.c
@@ -23,3 +23,41 @@ int rand(void) {
 void srand(unsigned int new_seed) {
 	seed = new_seed;
 }
+
+void* bsearch(const void* key, const void* base, size_t num, size_t size, int (*comp)(const void*, const void*)) {
+	size_t left = 0;
+	size_t right = num - 1;
+
+	while (left <= right) {
+		size_t middle = left + (right - left) / 2;
+		int comparison = comp(key, (char*)base + middle * size);
+
+		if (comparison == 0)
+			return (char*)base + middle * size;
+		else if (comparison < 0)
+			right = middle - 1;
+		else
+			left = middle + 1;
+	}
+
+	return NULL;
+}
+
+errno_t bsearch_s(const void* key, const void* base, size_t num, size_t size, int (*compare)(void*, const void*, const void*), void* context) {
+	size_t left = 0;
+	size_t right = num - 1;
+
+	while (left <= right) {
+		size_t middle = left + (right - left) / 2;
+		int comparison = compare(context, key, (char*)base + middle * size);
+
+		if (comparison == 0)
+			return 0;
+		else if (comparison < 0)
+			right = middle - 1;
+		else
+			left = middle + 1;
+	}
+
+	return 1;
+}
